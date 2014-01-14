186226a48b27ffb7a481301bf179681dc9cb24fc
ml-workshare <maurice.leong@gmail.com>
2014-01-14 15:12:37 +0000

    Fix failing unit test

--- src/com/dumbster/smtp/ClientSession.java
+++ src/com/dumbster/smtp/ClientSession.java
@@ -10,148 +10,148 @@ import com.workshare.dumbster.MailSocket;
 
 public class ClientSession implements Runnable {
 
-    private IOSource socket;
-    private volatile MailStore mailStore;
-    private MailMessage msg;
-    private Response smtpResponse;
-    private PrintWriter out;
-    private BufferedReader input;
-    private SmtpState smtpState;
-    private String line;
-    private String lastHeaderName = null;
+	private IOSource socket;
+	private volatile MailStore mailStore;
+	private MailMessage msg;
+	private Response smtpResponse;
+	private PrintWriter out;
+	private BufferedReader input;
+	private SmtpState smtpState;
+	private String line;
+	private String lastHeaderName = null;
 
-    public ClientSession(IOSource socket, MailStore mailStore) {
+	public ClientSession(IOSource socket, MailStore mailStore) {
 
-    	 this.socket = socket;
-         this.mailStore = mailStore;
-         this.msg = new MailMessageImpl();
-         Request request = Request.initialRequest();
-         smtpResponse = request.execute(this.mailStore, msg);
-    }
+		this.socket = socket;
+		this.mailStore = mailStore;
+		this.msg = new MailMessageImpl();
+		Request request = Request.initialRequest();
+		smtpResponse = request.execute(this.mailStore, msg);
+	}
 
-    public void run() {
-        try {
-            prepareSessionLoop();
-            sessionLoop();
-        } catch (Exception ignored) {
-        } finally {
-            try {
-                socket.close();
-            } catch (Exception ignored) {
-            }
-        }
-    }
+	public void run() {
+		try {
+			prepareSessionLoop();
+			sessionLoop();
+		} catch (Exception ignored) {
+		} finally {
+			try {
+				socket.close();
+			} catch (Exception ignored) {
+			}
+		}
+	}
 
-    private void prepareSessionLoop() throws IOException {
-        prepareOutput();
-        prepareInput();
-        sendResponse();
-        updateSmtpState();
-    }
+	private void prepareSessionLoop() throws IOException {
+		prepareOutput();
+		prepareInput();
+		sendResponse();
+		updateSmtpState();
+	}
 
-    private void prepareOutput() throws IOException {
-        out = socket.getOutputStream();
-        out.flush();
-    }
+	private void prepareOutput() throws IOException {
+		out = socket.getOutputStream();
+		out.flush();
+	}
 
-    private void prepareInput() throws IOException {
-        input = socket.getInputStream();
-    }
+	private void prepareInput() throws IOException {
+		input = socket.getInputStream();
+	}
 
-    private void sendResponse() {
-        if (smtpResponse.getCode() > 0) {
-            int code = smtpResponse.getCode();
-            String message = smtpResponse.getMessage();
-            out.print(code + " " + message + "\r\n");
-            out.flush();
-        }
-    }
+	private void sendResponse() {
+		if (smtpResponse.getCode() > 0) {
+			int code = smtpResponse.getCode();
+			String message = smtpResponse.getMessage();
+			out.print(code + " " + message + "\r\n");
+			out.flush();
+		}
+	}
 
-    private void updateSmtpState() {
-        smtpState = smtpResponse.getNextState();
-    }
+	private void updateSmtpState() {
+		smtpState = smtpResponse.getNextState();
+	}
 
-    private void sessionLoop() throws IOException {
-        while (smtpState != SmtpState.CONNECT && readNextLineReady()) {
-            Request request = Request.createRequest(smtpState, line);
-            smtpResponse = request.execute(mailStore, msg);
-            storeInputInMessage(request);
-            sendResponse();
-            updateSmtpState();
-            signalMessageReceivedIfComplete();
-            saveAndRefreshMessageIfComplete();
-        }
-    }
+	private void sessionLoop() throws IOException {
+		while (smtpState != SmtpState.CONNECT && readNextLineReady()) {
+			Request request = Request.createRequest(smtpState, line);
+			smtpResponse = request.execute(mailStore, msg);
+			storeInputInMessage(request);
+			sendResponse();
+			updateSmtpState();
+			signalMessageReceivedIfComplete();
+			saveAndRefreshMessageIfComplete();
+		}
+	}
 
-    private boolean readNextLineReady() throws IOException {
-        readLine();
-        return line != null;
-    }
+	private boolean readNextLineReady() throws IOException {
+		readLine();
+		return line != null;
+	}
 
-    private void readLine() throws IOException {
-        line = input.readLine();
-    }
+	private void readLine() throws IOException {
+		line = input.readLine();
+	}
 
-    private void saveAndRefreshMessageIfComplete() {
-        if (smtpState == SmtpState.QUIT) {
-            mailStore.addMessage(msg);
-            msg = new MailMessageImpl();
-        }
-    }
-    
-    private void signalMessageReceivedIfComplete() {
-    	if (smtpState == SmtpState.QUIT) {
-    		MailData data = new MailData(msg);
-    		String hostname = data.getHeader(Constants.CallbackHeader);
-    		String portvalue = data.getHeader(Constants.CallbackPortHeader);
-    		
-    		if( hostname != null && portvalue != null ) {
-    			
-    		}
-    			int port = Integer.parseInt(portvalue);
-        		try {
-        			
-    				MailSocket.openSocketAndWrite(hostname, port, data);
-    				
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    		}
-    	}
-    }
+	private void saveAndRefreshMessageIfComplete() {
+		if (smtpState == SmtpState.QUIT) {
+			mailStore.addMessage(msg);
+			msg = new MailMessageImpl();
+		}
+	}
 
-    private void storeInputInMessage(Request request) {
-        String params = request.getParams();
-        if (null == params)
-            return;
+	private void signalMessageReceivedIfComplete() {
+		if (smtpState == SmtpState.QUIT) {
+			MailData data = new MailData(msg);
+			String hostname = data.getHeader(Constants.CallbackHeader);
+			String portvalue = data.getHeader(Constants.CallbackPortHeader);
 
-        if (SmtpState.DATA_HDR.equals(smtpResponse.getNextState())) {
-            addDataHeader(params);
-            return;
-        }
+			if( hostname != null && portvalue != null ) {
 
-        if (SmtpState.DATA_BODY == smtpResponse.getNextState()) {
-            msg.appendBody(params);
-            return;
-        }
-    }
+				int port = Integer.parseInt(portvalue);
+				try {
 
-    private void addDataHeader(String params) {
-        int headerNameEnd = params.indexOf(':');
-        if (headerNameEnd > 0 && !whiteSpacedLineStart(params)) {
-            lastHeaderName = params.substring(0, headerNameEnd).trim();
-            String value = params.substring(headerNameEnd + 1).trim();
-            msg.addHeader(lastHeaderName, value);
-        } else if (whiteSpacedLineStart(params) && lastHeaderName != null) {
-            msg.appendHeader(lastHeaderName, params);
-        }
-    }
+					MailSocket.openSocketAndWrite(hostname, port, data);
 
-    private boolean whiteSpacedLineStart(String s)  {
-        if (s == null || "".equals(s))
-          return false;
-        char c = s.charAt(0);
-        return c == 32 || c == 0x0b || c == '\n' ||
-                c == '\r' || c == '\t' || c == '\f';
-    }
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void storeInputInMessage(Request request) {
+		String params = request.getParams();
+		if (null == params)
+			return;
+
+		if (SmtpState.DATA_HDR.equals(smtpResponse.getNextState())) {
+			addDataHeader(params);
+			return;
+		}
+
+		if (SmtpState.DATA_BODY == smtpResponse.getNextState()) {
+			msg.appendBody(params);
+			return;
+		}
+	}
+
+	private void addDataHeader(String params) {
+		int headerNameEnd = params.indexOf(':');
+		if (headerNameEnd > 0 && !whiteSpacedLineStart(params)) {
+			lastHeaderName = params.substring(0, headerNameEnd).trim();
+			String value = params.substring(headerNameEnd + 1).trim();
+			msg.addHeader(lastHeaderName, value);
+		} else if (whiteSpacedLineStart(params) && lastHeaderName != null) {
+			msg.appendHeader(lastHeaderName, params);
+		}
+	}
+
+	private boolean whiteSpacedLineStart(String s)  {
+		if (s == null || "".equals(s))
+			return false;
+		char c = s.charAt(0);
+		return c == 32 || c == 0x0b || c == '\n' ||
+				c == '\r' || c == '\t' || c == '\f';
+	}
 
 }
